0b0c04695c930742e71f14f98def64f53f034d52
deepak <deepak@withstartups.com>
2014-09-01 11:37:38 +0530

    completed exercises for objects

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,25 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents){
+  return {title: courseTitle, duration: courseDuration, students: courseStudents}
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object, newProp, newValue){
+  if (newProp in object){
+    return object;
+  } else {
+    object[newProp] = newValue;
+    return object;
+  }
+};
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(letter){
+  return "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender;
+};
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item, money){
+  var priceTable = {'MacBook Air': 999, 'MacBook Pro': 1299, 'Mac Pro': 2499, 'Apple Sticker':1};
+  if (priceTable[item] <= money){
+    return true;
+  } else {
+    return false;
+  }
+};  
